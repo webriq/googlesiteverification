0a338bbe7f3a6d2a27b4a8a36aa448f722cbc09a
David Pozsar <david.pozsar@megaweb>
2013-10-31 14:56:13 +0100

    modules disabled by default after installation

--- sql/grid-googlesiteverification/site/data.0-0.1.sql
+++ sql/grid-googlesiteverification/site/data.0-0.1.sql
@@ -1,4 +1,4 @@
 -- insert default values for table: module
 
 INSERT INTO "module" ( "module", "enabled" )
-     VALUES ( 'Grid\GoogleSiteVerification', TRUE );
+     VALUES ( 'Grid\GoogleSiteVerification', FALSE );
